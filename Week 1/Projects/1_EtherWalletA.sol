4013e27482590b8186341c45551a09e8ce6e5799
Basit <basitabdulkareem@gmail.com>
2022-10-31 19:24:43 +0000

    Ether A work

--- Week 1/Projects/1_EtherWalletA.sol
+++ Week 1/Projects/1_EtherWalletA.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
+//In this contract anyone can deposit, the deposit goes directly to the blockchain
 contract EtherWallet {
     address payable public owner;
+    uint256 private balance;
 
     constructor() {
         owner = payable(msg.sender);
@@ -11,15 +13,15 @@ contract EtherWallet {
     // receive() external payable {}
 
     function deposit() external payable {
-        
+        balance += msg.value;
     }
 
-    function withdraw(uint _amount) external {
+    function withdraw(uint256 _amount) external {
         require(msg.sender == owner, "caller is not owner");
         payable(msg.sender).transfer(_amount);
     }
 
-    function getBalance() external view returns (uint) {
+    function getBalance() external view returns (uint256) {
         return address(this).balance;
     }
 }
